789e385d61624c1cdb4ef7ac9b2c8840d6104e0a
Martin Macharia <mosmartin5@gmail.com>
2022-11-01 00:04:37 +0100

    :seedling: Setup the project

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: './public/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
